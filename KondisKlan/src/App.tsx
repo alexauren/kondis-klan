7aeb5c615d41e83a048be856675280c459fe2c89
Kristian <Kristian@Skrede.info>
2023-02-10 12:53:42 +0100

    feat: first demo of WorkoutCard

--- KondisKlan/src/App.tsx
+++ KondisKlan/src/App.tsx
@@ -6,6 +6,7 @@ import { firebaseConfig } from './firebase/firebaseConfig'
 import { getFirestore } from 'firebase/firestore'
 import { LandingPage } from './modules/landing/LandingPage'
 import { ExampleForm } from './modules/workoutsession/forms/ExampleForm'
+import { WorkoutCard } from './modules/workoutsession/workoutcard/WorkoutCard'
 
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app);
@@ -16,6 +17,18 @@ function App() {
     <div className="App">
       <LandingPage />
       <ExampleForm/>
+      <WorkoutCard 
+        //use image url from firebase
+        image={"https://www.gymgrossisten.no/on/demandware.static/-/Sites-Gymgrossisten-Library/default/dwfe1a19d3/page_designer_articles/slik-blir-du-bedre-i-benkpress2.jpg"}
+        title="Benkpress" 
+        description="Benkpress er en øvelse som trener bryst, skuldre og triceps." 
+        stats={[
+          { title: 'Sets', value: '3' },
+          { title: 'Reps', value: '5' },
+          { title: 'Vekt', value: '100kg' },
+
+        ]}
+      />
     </div>
   )
 }
---
+++ KondisKlan/src/modules/workoutsession/workoutcard/WorkoutCard.tsx
@@ -0,0 +1,63 @@
+import { createStyles, Card, Image, Text, Group, RingProgress } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  card: {
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+  },
+
+  footer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: `${theme.spacing.sm}px ${theme.spacing.lg}px`,
+    borderTop: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
+    }`,
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    lineHeight: 1,
+  },
+}));
+
+interface WorkoutCard {
+  image: string;
+  title: string;
+  description: string;
+  stats: {
+    title: string;
+    value: string;
+  }[];
+}
+
+export function WorkoutCard({ image, title, description, stats }: WorkoutCard) {
+  const { classes } = useStyles();
+
+  const items = stats.map((stat) => (
+    <div key={stat.title}>
+      <Text size="xs" color="dimmed">
+        {stat.title}
+      </Text>
+      <Text weight={500} size="sm">
+        {stat.value}
+      </Text>
+    </div>
+  ));
+
+  return (
+    <Card withBorder p="lg" className={classes.card} mt={200} >
+      <Card.Section>
+        <Image src={image} alt={title} height={200} />
+      </Card.Section>
+
+      <Group position="apart" mt="xl">
+        <Text size="sm" weight={700} className={classes.title}>
+          {title}
+        </Text>
+      </Group>
+      <Text mt="sm" mb="md" color="dimmed" size="xs">
+        {description}
+      </Text>
+      <Card.Section className={classes.footer}>{items}</Card.Section>
+    </Card>
+    ) }
