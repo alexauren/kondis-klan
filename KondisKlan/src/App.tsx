94812191f7aceaf3d57021124d14117df400d4df
Kristian <Kristian@Skrede.info>
2023-02-14 09:31:16 +0100

    WorkoutCard: Finished first version

--- KondisKlan/src/App.tsx
+++ KondisKlan/src/App.tsx
@@ -6,7 +6,6 @@ import { firebaseConfig } from './firebase/firebaseConfig'
 import { getFirestore } from 'firebase/firestore'
 import { LandingPage } from './modules/landing/LandingPage'
 import { ExampleForm } from './modules/workoutsession/forms/ExampleForm'
-import { WorkoutCard } from './modules/workoutsession/workoutcard/WorkoutCard'
 
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app);
@@ -17,18 +16,6 @@ function App() {
     <div className="App">
       <LandingPage />
       <ExampleForm/>
-      <WorkoutCard 
-        //use image url from firebase
-        image={"https://www.gymgrossisten.no/on/demandware.static/-/Sites-Gymgrossisten-Library/default/dwfe1a19d3/page_designer_articles/slik-blir-du-bedre-i-benkpress2.jpg"}
-        title="Benkpress" 
-        description="Benkpress er en øvelse som trener bryst, skuldre og triceps." 
-        stats={[
-          { title: 'Sets', value: '3' },
-          { title: 'Reps', value: '5' },
-          { title: 'Vekt', value: '100kg' },
-
-        ]}
-      />
     </div>
   )
 }
--- KondisKlan/src/modules/workoutsession/workoutcard/WorkoutCard.tsx
+++ KondisKlan/src/modules/workoutsession/workoutcard/WorkoutCard.tsx
@@ -1,25 +1,16 @@
 import { createStyles, Card, Image, Text, Group, RingProgress } from '@mantine/core';
-
+// CSS
 const useStyles = createStyles((theme) => ({
-  card: {
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
-  },
-
   footer: {
     display: 'flex',
     justifyContent: 'space-between',
     padding: `${theme.spacing.sm}px ${theme.spacing.lg}px`,
-    borderTop: `1px solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
-    }`,
-  },
-
-  title: {
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    lineHeight: 1,
+    borderTop: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
+      }`,
   },
 }));
 
+//interface
 interface WorkoutCard {
   image: string;
   title: string;
@@ -30,12 +21,12 @@ interface WorkoutCard {
   }[];
 }
 
+//component
 export function WorkoutCard({ image, title, description, stats }: WorkoutCard) {
   const { classes } = useStyles();
-
   const items = stats.map((stat) => (
     <div key={stat.title}>
-      <Text size="xs" color="dimmed">
+      <Text>
         {stat.title}
       </Text>
       <Text weight={500} size="sm">
@@ -45,19 +36,33 @@ export function WorkoutCard({ image, title, description, stats }: WorkoutCard) {
   ));
 
   return (
-    <Card withBorder p="lg" className={classes.card} mt={200} >
+    <Card withBorder p="md" mt={200} >
       <Card.Section>
         <Image src={image} alt={title} height={200} />
       </Card.Section>
 
-      <Group position="apart" mt="xl">
-        <Text size="sm" weight={700} className={classes.title}>
-          {title}
-        </Text>
-      </Group>
-      <Text mt="sm" mb="md" color="dimmed" size="xs">
+      <Text size="sm" weight={700}>
+        {title}
+      </Text>
+
+      <Text mt="sm" mb="md" size="xs">
         {description}
       </Text>
       <Card.Section className={classes.footer}>{items}</Card.Section>
     </Card>
-    ) }
+  )
+}
+
+// Example
+//import { WorkoutCard } from './modules/workoutsession/workoutcard/WorkoutCard'
+{/* <WorkoutCard
+image={"https://www.gymgrossisten.no/on/demandware.static/-/Sites-Gymgrossisten-Library/default/dwfe1a19d3/page_designer_articles/slik-blir-du-bedre-i-benkpress2.jpg"}
+title="Benkpress"
+description="Benkpress er en øvelse som trener bryst, skuldre og triceps."
+stats={[
+  { title: 'Sets', value: '3' },
+  { title: 'Reps', value: '5' },
+  { title: 'Vekt', value: '100kg' },
+]}
+/>
+<WorkoutCard */}
