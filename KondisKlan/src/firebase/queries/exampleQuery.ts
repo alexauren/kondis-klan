988928ebc56ca2dc328bd7550b5825b6763d7109
plebbimon <sebastiansmaland@gmail.com>
2023-02-14 12:45:37 +0100

    chore: removed redundant functions inside `exampleQuery`

--- KondisKlan/src/firebase/queries/exampleQuery.ts
+++ KondisKlan/src/firebase/queries/exampleQuery.ts
@@ -5,17 +5,3 @@ import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { db } from '../../App'
 
 export const exampleDocRef = collection(db, "workoutsessions");
-
-function useExerciseCollection(documentRef: string) {
-    //use useCollectionData to get the data from the collection
-    const [data, loading, error] = useCollectionData(
-        collection(db, documentRef));
-    return { data, loading, error };
-}
-
-function useExerciseDocument(collectionRef: string) {
-    //use useDocumentData to get the data from the document
-    const [data, loading, error] = useCollectionData(
-        collection(db, collectionRef));
-    return { data, loading, error };
-}
