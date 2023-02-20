b310daa925eab7922755dea6255d873514b28c99
Kristian <Kristian@Skrede.info>
2023-02-20 14:21:21 +0100

    chore: cleanup and added example

--- KondisKlan/src/modules/newprogram/NewProgram.tsx
+++ KondisKlan/src/modules/newprogram/NewProgram.tsx
@@ -1,87 +1,103 @@
-import { useState, useEffect } from "react";
-import { Stepper, Button, Group, Modal, TextInput } from "@mantine/core";
-import { useForm } from "@mantine/form";
-import { DatePicker } from "@mantine/dates";
+import { useState, useEffect } from 'react'
+import {
+  Stepper,
+  Button,
+  Group,
+  Modal,
+  TextInput,
+  Card,
+  Title,
+} from '@mantine/core'
+import { useForm } from '@mantine/form'
+import { DatePicker } from '@mantine/dates'
 
 export function NewProgram() {
-  const [active, setActive] = useState(0);
+  const [active, setActive] = useState(0)
   const nextStep = () =>
-    setActive((current) => (current < 2 ? current + 1 : current));
+    setActive(current => (current < 2 ? current + 1 : current))
   const prevStep = () =>
-    setActive((current) => (current > 0 ? current - 1 : current));
-  const [opened, setOpened] = useState(false);
-  const [isValid, setIsValid] = useState(false);
+    setActive(current => (current > 0 ? current - 1 : current))
+  const [opened, setOpened] = useState(false)
+  const [isValid, setIsValid] = useState(false)
 
   const form = useForm({
     //data for testing purposes
     //initialValues: { name: "", email: "", Date: "" },
     initialValues: {
-      name: "Workout",
-      email: "john@smith.com",
+      name: 'Workout',
+      email: 'john@smith.com',
       Date: new Date(),
     },
     validateInputOnBlur: true,
     validate: {
-      name: (value) =>
-        value.length < 2 ? "Name must have at least 2 letters" : null,
-      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      name: value =>
+        value.length < 2 ? 'Name must have at least 2 letters' : null,
+      email: value => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
     },
-  });
+  })
 
   useEffect(() => {
-    setIsValid(Object.keys(form.errors).length === 0);
-  }, [form.errors]);
+    setIsValid(Object.keys(form.errors).length === 0)
+  }, [form.errors])
 
   return (
     <>
-      <Stepper active={active} onStepClick={setActive} breakpoint="sm">
-        <Stepper.Step label="First step" description="Create a new program">
-          <form>
-            <TextInput
-              withAsterisk
-              label="Name of program"
-              placeholder="Push, Pull, Legs"
-              {...form.getInputProps("name")}
-            />
-            <TextInput
-              mt="sm"
-              label="Createdby (will be auto filled)"
-              placeholder="john@smith.com"
-              {...form.getInputProps("email")}
-            />
-            <DatePicker
-              label="Created date"
-              placeholder="Choose date"
-              {...form.getInputProps("Date")}
-            />
-          </form>
-        </Stepper.Step>
-        <Stepper.Step label="Second step" description="Choose exercises">
-          <Button variant="outline" onClick={() => setOpened(true)}>
-            Choose exercises
-          </Button>
-          <Modal
-            title="Choose exercises"
-            opened={opened}
-            onClose={() => setOpened(false)}
-          >
-            *formet til Lasse*
-          </Modal>
-        </Stepper.Step>
-        <Stepper.Completed>
-          Completed, click back button edit your program. registred data:
-          {JSON.stringify(form.values, null, 2)}
-        </Stepper.Completed>
-      </Stepper>
+      <Card withBorder shadow={'sm'}>
+        <Title> New Program</Title>
+        <Stepper
+          active={active}
+          onStepClick={setActive}
+          breakpoint="sm"
+          mt="md"
+        >
+          <Stepper.Step label="First step" description="Create a new program">
+            <form>
+              <TextInput
+                withAsterisk
+                label="Name of program"
+                placeholder="Push, Pull, Legs"
+                {...form.getInputProps('name')}
+              />
+              <TextInput
+                mt="sm"
+                label="Createdby (will be auto filled)"
+                placeholder="john@smith.com"
+                {...form.getInputProps('email')}
+              />
+              <DatePicker
+                label="Created date"
+                placeholder="Choose date"
+                {...form.getInputProps('Date')}
+              />
+            </form>
+          </Stepper.Step>
+          <Stepper.Step label="Second step" description="Choose exercises">
+            <Button variant="outline" onClick={() => setOpened(true)}>
+              Choose exercises
+            </Button>
+            <Modal
+              title="Choose exercises"
+              opened={opened}
+              onClose={() => setOpened(false)}
+            >
+              *formet til Lasse*
+            </Modal>
+          </Stepper.Step>
+          <Stepper.Completed>
+            Completed, click back button edit your program. registred data:
+            {JSON.stringify(form.values, null, 2)}
+          </Stepper.Completed>
+        </Stepper>
 
-      <Group position="center" mt="xl">
-        <Button variant="default" onClick={prevStep}>
-          Back
-        </Button>
-        <Button onClick={nextStep} disabled={!isValid}>
-          {active === 2 ? "Finish" : "Next step"}
-        </Button>
-      </Group>
+        <Group position="center" mt="xl">
+          <Button variant="default" onClick={prevStep}>
+            Back
+          </Button>
+          <Button onClick={nextStep} disabled={!isValid}>
+            {active === 2 ? 'Finish' : 'Next step'}
+          </Button>
+        </Group>
+      </Card>
     </>
-  );
+  )
 }
--- KondisKlan/src/modules/workoutsession/views/WorkoutSessionDetail.tsx
+++ KondisKlan/src/modules/workoutsession/views/WorkoutSessionDetail.tsx
@@ -1,10 +1,12 @@
 import { Stack } from '@mantine/core'
+import { NewProgram } from 'modules/newprogram/NewProgram'
 import { WorkoutSessionFeed } from 'modules/workoutsession/components/WorkoutSessionFeed'
 
 export function WorkoutSessionDetail() {
   return (
     <Stack>
       <h2>Velkommen tilbake!</h2>
+      <NewProgram />
       <WorkoutSessionFeed />
     </Stack>
   )
